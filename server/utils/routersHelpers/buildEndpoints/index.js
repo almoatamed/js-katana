4ad94cfd9437e935d42a6f40d9d36af7a831ba67
Salem Elmotamed <s.elmotamed96@gmail.com>
2025-04-14 01:47:44 +0200

    simple log

--- server/utils/routersHelpers/buildEndpoints/index.js
+++ server/utils/routersHelpers/buildEndpoints/index.js
@@ -9,11 +9,14 @@ function routesLister(router, path = "", routes = endPoints, root = true) {
     if (router.stack) {
         for (const layer of router.stack) {
             if (layer.route) {
+                const routePath = `${path}${layer.route.path}`.replace("\\", "")
                 routes.push({
                     methods: layer.route.methods,
-                    path: `${path}${layer.route.path}`.replace("\\", ""),
+                    path: routePath,
                     handlersName: layer.route.stack.map((handler) => handler?.name),
                 });
+                console.log("Route", routePath)
+
             }
             if (layer?.handle?.stack) {
                 const subPath = String(layer.regexp)
