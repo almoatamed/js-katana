da2defa46d331b3a6b8eb094399a82d00c69d3f5
S. A. Elmotamed <s.elmotamed96@gmail.com>
2025-04-20 00:30:27 +0200

    removed cli

--- server/utils/cli/commands/db/index.ts
+++
@@ -1,7 +0,0 @@
-const runSubCommandLine = (await import("./handle/index.js")).runSubCommandLine;
-
-const createCommand = (program: import("commander").Command) => {
-    const dbCommand = program.command("database").alias("db").description("database set of commands");
-    runSubCommandLine(dbCommand);
-};
-export { createCommand };
--- server/utils/cli/utils/srcPath/index.ts
+++
@@ -1,5 +0,0 @@
-import path from "path";
-import url from "url";
-
-const srcPath: string = path.resolve(path.join(path.dirname(url.fileURLToPath(import.meta.url)), "../../../../."));
-export { srcPath };
