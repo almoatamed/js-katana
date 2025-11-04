18d5e80f6e031b1af8f990af374151bc14087a7a
Salem Elmotamed <s.elmotamed96@gmail.com>
2025-11-04 04:00:34 +0200

    remove stuff

--- server/utils/crypto/hash.ts
+++
@@ -1,5 +0,0 @@
-import { createHash } from "node:crypto";
-
-export const hashWithSHA256 = (data: string) => {
-    return createHash("sha256").update(data).digest("hex");
-};
--- server/utils/redis/index.ts
+++
@@ -1,8 +0,0 @@
-import { Redis } from "ioredis";
-import { redisConfig } from "../../config/redis/index.js";
-
-export const redisClient = new Redis({
-    password: redisConfig.getRedisPassword(),
-    host: redisConfig.getRedisHost(),
-    port: redisConfig.getRedisPort(),
-});
