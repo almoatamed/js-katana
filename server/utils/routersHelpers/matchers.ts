6a8b95951f167c008e09618c1e5912ee08509d8b
Salem Elmotamed <s.elmotamed96@gmail.com>
2025-11-08 05:54:22 +0200

    `Update package.json version to 1.0.4 and update routerSuffixRegx in matchers.ts`

--- server/utils/routersHelpers/matchers.ts
+++ server/utils/routersHelpers/matchers.ts
@@ -6,7 +6,7 @@ export const directoryAliasSuffixRegx = RegExp(
     (await valueOf(routerConfig.getDirectoryAliasSuffixRegx)) || "\\.directoryAlias\\.(?:js|ts)$"
 );
 
-export const routerSuffixRegx = RegExp((await valueOf(routerConfig.getRouterSuffixRegx)) || "\\.router\\.(?:js|ts)$");
+export const routerSuffixRegx = RegExp((await valueOf(routerConfig.getRouterSuffixRegx)) || "\\.router\\.(?:js|ts)x?$");
 export const descriptionSuffixRegx = RegExp(
     (await valueOf(routerConfig.getDescriptionSuffixRegx)) || "\\.description\\.[a-zA-Z]{1,10}$"
 );
